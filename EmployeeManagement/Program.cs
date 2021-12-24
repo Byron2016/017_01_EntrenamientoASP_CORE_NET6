f8820cd5e8a0acf665c4e9e7930fc1e783da6c92
Byron 2016 <bgva2005@yahoo.com>
2021-12-24 08:11:39 -0500

    feat: v9 4.21 show appsetting key

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,7 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// app.MapGet("/", () => "Hello World!");
 // app.MapGet("/", () => System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+// forma 1
+var myKey = builder.Configuration["MyKey"];
+var myKeya = builder.Configuration.GetValue<string>("MyKey");
+var cookieAuthenticationLoginPath = builder.Configuration.GetValue<string>("Authentication:CookieAuthentication:LoginPath");
+// forma 2
+IConfiguration conf = app.Configuration;
+var p = conf["MyKey"];
+// forma 3 NO HACERLO TIENE WARNING.
+var provider = builder.Services.BuildServiceProvider();
+var configuration = provider.GetRequiredService<IConfiguration>();
+var myKeyb = configuration.GetValue<string>("MyKey");
+
+app.MapGet("/", () => $"mykey: {myKey} mykeya: {myKeya} cookieAuthenticationLoginPath: {cookieAuthenticationLoginPath} p: {p} myKeyb: {myKeyb}");
 
 app.Run();
